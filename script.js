82b04ac8cf42b526c39d435e5e679d279a3e88eb
Rakesh Patel <rakesh246808@gmail.com>
2023-10-28 07:15:33 +0530

    day-3

--- script.js
+++ script.js
@@ -28,13 +28,13 @@ darkMoodBtn.onclick = () => {
   
     if(rk==0)
     {
-        document.body.classList.add('darkmoodbtn');
+        document.btn.classList.add('darkmoodbtn');
         rk = 1;
     }
 
     else
     {
-        document.body.classList.remove('darkmoodbtn');
+        document.btn.classList.remove('darkmoodbtn');
         rk = 0;
     }
     
@@ -56,6 +56,57 @@ secondPageBtn.onclick = () => {
 secondPageBtn.addEventListener('onclick',function() {
     console.log("rakesh");
 });
+
+
+
+
+
+
+
+
+// cusrsor Selection   ********************
+
+let div = document.querySelector('#cursor');
+let button = document.querySelector('#btns');
+
+document.onmousemove = function(e) {
+    //move cursor *********
+    console.log("rakesh");
+   cursor.style.top = e.pageY + 'px';
+   cursor.style.left = e.pageX + 'px';
+  
+//    animate background  
+button.style.backgroundPositionX = e.pageX/-4 + 'px';
+button.style.backgroundPositionY = e.pageY/-4 + 'px';
+
+//add elements to btn
+let element =  document.createElement('span');
+element.className = 'element';
+button.prepend(element);
+console.log(element);
+
+// move elements randomly across x and y direction  
+element.style.left = cursor.getBoundingClientRect().x - 35 + 'px';
+element.style.top = cursor.getBoundingClientRect().y - 10 + 'px';
+
+setTimeout(function() {
+    let text = document.querySelectorAll('.element')[0],
+    directionX = Math.random() < .5 ? -1 : 1,
+    directionY = Math.random() < .5 ? -1 : 1
+
+    text.style.left = parseInt(text.style.left) - (directionX * (Math.random() * 200)) + 'px';
+    text.style.top = parseInt(text.style.left) - (directionY * (Math.random() * 200)) + 'px';
+    text.style.opacity = 0;
+    text.style.transform = 'scale(.25)';
+
+
+    // remove element  
+    setTimeout(function(){
+        element.remove();
+    },400)
+
+},10)
+}
 
 
 
