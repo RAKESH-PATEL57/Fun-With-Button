ca18d85acd9b8eab80f77e455530e636848eaaca
Rakesh Patel <rakesh246808@gmail.com>
2023-10-29 07:59:31 +0530

    day-4

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ firstBtn.addEventListener('mouseover', function() {
 });
 
 secondBtn.addEventListener('mouseover', function() {
-    if((firstBtn.innerHTML='Yes') || (secondBtn.innerHTML='No')) {
+    if((firstBtn.innerText='Yes') || (secondBtn.innerHTML='No')) {
         firstBtn.innerHTML='No';
         secondBtn.innerHTML='Yes';
     }
@@ -54,7 +54,7 @@ secondPageBtn.onclick = () => {
 }
 
 secondPageBtn.addEventListener('onclick',function() {
-    console.log("rakesh");
+  
 });
 
 
@@ -69,25 +69,22 @@ secondPageBtn.addEventListener('onclick',function() {
 let div = document.querySelector('#cursor');
 let button = document.querySelector('#btns');
 
-document.onmousemove = function(e) {
+button.onmousemove = function(e) {
     //move cursor *********
-    console.log("rakesh");
-   cursor.style.top = e.pageY + 'px';
-   cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+    cursor.style.left = e.pageX + 'px';
   
-//    animate background  
-button.style.backgroundPositionX = e.pageX/-4 + 'px';
-button.style.backgroundPositionY = e.pageY/-4 + 'px';
-
 //add elements to btn
 let element =  document.createElement('span');
 element.className = 'element';
 button.prepend(element);
-console.log(element);
 
 // move elements randomly across x and y direction  
-element.style.left = cursor.getBoundingClientRect().x -30 + 'px';
+// element.style.left = cursor.getBoundingClientRect().x - 40 + 'px';
+// element.style.right = cursor.getBoundingClientRect().y + 40 + 'px';
 element.style.top = cursor.getBoundingClientRect().y - 368 + 'px';
+element.style.right = cursor.getBoundingClientRect().x + 368 + 'px';
+console.log(element.style.top);
 
 setTimeout(function() {
     let text = document.querySelectorAll('.element')[0],
@@ -97,8 +94,7 @@ setTimeout(function() {
     text.style.left = parseInt(text.style.left) - (directionX * (Math.random() * 200)) + 'px';
     text.style.top = parseInt(text.style.left) - (directionY * (Math.random() * 200)) + 'px';
     text.style.opacity = 0;
-    text.style.transform = 'scale(.25)';
-
+    text.style.transform = 'scale(.15)';
 
     // remove element  
     setTimeout(function(){
